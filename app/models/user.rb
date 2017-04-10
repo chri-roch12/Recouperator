e79f13000afba512ecfb0c89a962c460f38da74a
chri-roch12 <chris.prochnow@gmail.com>
2017-04-10 15:41:55 -0700

    Role is now correctly generated for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  enum role: [:patient, :clinician]
+  enum role: [:unspecified, :patient, :clinician]
 end
