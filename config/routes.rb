26b2689a5ae5d6335a264a71483a1c03181d4932
chri-roch12 <chris.prochnow@gmail.com>
2017-04-10 17:40:46 -0700

    Added treatment routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users, only: [:index] do
+    resources :treatments
 
 
 end
